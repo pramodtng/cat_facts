c0fa1abd1e5d64099a45a82f01874fb8d1b14b14
pramodtng <psingay96@gmail.com>
2023-09-29 00:42:04 +0600

    initial-commit

---
+++ components/CatComponents.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const CatComponents = ({ result, currentPage, renderPaginationLinks }) => {
+  console.log(result);
+  return (
+    <div className="container max-w-6xl mx-auto px-2 py-36 relative z-10">
+      <div className="row">
+        <div className="col-12 p-2 text-justify">
+          <p className="text-2xl text-black pb-4 font-poppins">Cat Facts</p>
+          <p className='font-poppins'>Collections of some of the cat facts</p>
+          {result.map(function (facts) {
+            return (
+              <div key={facts.id}>
+                <div className="hs-accordion-group py-2">
+                  <div
+                    className="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6  active"
+                    id={`hs-basic-with-title-and-arrow-stretched-heading-${facts.id}`}
+                  >
+                    <div
+                      id={`hs-basic-with-title-and-arrow-stretched-collapse-${facts.id}`}
+                      className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+                      aria-labelledby={`hs-basic-with-title-and-arrow-stretched-heading-${facts.id}`}
+                    >
+                      <p className="text-gray-500">{facts.fact}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <nav aria-label="Pagination" className="flex justify-center items-center text-gray-600 mt-8 lg:mt-0 font-poppins">
+        {renderPaginationLinks()}
+      </nav>
+      </div>
+    </div>
+  );
+};
+
+export default CatComponents;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif']
     },
+    extend: {}
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
